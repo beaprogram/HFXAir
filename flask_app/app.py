b2f6a3ca689e52a4036f2ef634620d99b231de63
harsh <vn328490@dal.ca>
2025-11-05 23:59:09 -0400

    Feature - login Test case green hardcoded route

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,10 +1,12 @@
-from flask import Flask
-
+# app.py
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello, World!"
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# login route
+@app.post("/login")
+def login():
+    data = request.json
+    if data["flight_number"] == "AC123" and data["ticket_number"] == "TCK5678":
+        return jsonify({"token": "fake_token"}), 200
+    return jsonify({"error": "Invalid flight or ticket"}), 401
