2ece2f5fdb22577217cd08d8e4793633e64aebe3
dhgajjar <ay529022@dal.ca>
2025-11-08 14:23:49 -0400

    - Added: Firebase implementation for push notification.

--- reactnative/app/_layout.tsx
+++ reactnative/app/_layout.tsx
@@ -1,26 +1,62 @@
-import React, { useState } from 'react';
-import { View } from 'react-native';
-import LoginScreen from './login';
-import LoadingScreen from './loading';
-import GuestFlightsScreen from './guest-flight';
-import HomeScreen from './home';
+import React, { useEffect, useState } from "react";
+import { Alert, PermissionsAndroid, View } from "react-native";
+import LoginScreen from "./login";
+import LoadingScreen from "./loading";
+import GuestFlightsScreen from "./guest-flight";
+import HomeScreen from "./home";
+import messaging from "@react-native-firebase/messaging";
 
 export default function RootLayout() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [isGuest, setIsGuest] = useState(false);
-  const [userData, setUserData] = useState<{ticketNumber: string, flightNumber: string} | null>(null);
+  const [userData, setUserData] = useState<{
+    ticketNumber: string;
+    flightNumber: string;
+  } | null>(null);
+
+  const requestPermissions = async () => {
+    try {
+      const result = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS
+      );
+      if (result === PermissionsAndroid.RESULTS.GRANTED) {
+        await requestToken();
+      } else {
+        Alert.alert(
+          "Permission Denied",
+          "Notification permissions were denied."
+        );
+      }
+    } catch (error) {
+      console.log("Permission request failed:", error);
+    }
+  };
+
+  const requestToken = async () => {
+    try {
+      await messaging().registerDeviceForRemoteMessages();
+      const token = await messaging().getToken();
+      console.log("FCM Token:", token);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    requestPermissions();
+  }, []);
 
   const handleLogin = (ticketNumber: string, flightNumber: string) => {
     setIsLoading(true);
-    
-    const guestLogin = ticketNumber === '' && flightNumber === '';
-    
+
+    const guestLogin = ticketNumber === "" && flightNumber === "";
+
     setTimeout(() => {
       setIsLoading(false);
       setIsAuthenticated(true);
       setIsGuest(guestLogin);
-      
+
       if (!guestLogin) {
         setUserData({ ticketNumber, flightNumber });
       }
