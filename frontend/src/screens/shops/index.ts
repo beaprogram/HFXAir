6b3596a5903eea06f8adc5623de36aa98fd64216
mundhiyara <ai528793@dal.ca>
2025-11-27 15:51:13 -0400

    -tested components

--- frontend/src/screens/shops/index.ts
+++ frontend/src/screens/shops/index.ts
@@ -2,3 +2,4 @@ export { default as ShopListView } from './ShopListView';
 export { default as ShopDetailView } from './ShopDetailView';
 export { default as CatalogView } from './CatalogView';
 export { default as ItemDetailView } from './ItemDetailView';
+export { default as MyReservationsView } from './MyReservationsView';
