8a73873def7e78d326870ae30d116a9af3a6c3c0
mundhiyara <ai528793@dal.ca>
2025-11-27 15:50:13 -0400

    -resolved bugs

--- frontend/src/screens/shops/index.ts
+++ frontend/src/screens/shops/index.ts
@@ -2,4 +2,3 @@ export { default as ShopListView } from './ShopListView';
 export { default as ShopDetailView } from './ShopDetailView';
 export { default as CatalogView } from './CatalogView';
 export { default as ItemDetailView } from './ItemDetailView';
-export { default as MyReservationsView } from './MyReservationsView';
